0d162d02357f90f8c13e4e3436e8fd063aa80741
osama-abdullah <osama2kabdullah@gmail.com>
2022-09-27 11:31:21 +0600

    delete system added

--- src/pages/SelectedTopic.js
+++ src/pages/SelectedTopic.js
@@ -11,17 +11,34 @@ const SelectedTopic = () => {
       const loadUserSelectedData = async () => {
         const { data } = await axios.get(
           "https://sleepy-brushlands-75204.herokuapp.com/selectedtopics?email=" +
-            user?.email, {
-                headers: {
-                    authorization: `Bearer ${localStorage.getItem('accessToken')}`
-                }
-            }
+            user?.email,
+          {
+            headers: {
+              authorization: `Bearer ${localStorage.getItem("accessToken")}`,
+            },
+          }
         );
         setDatas(data);
       };
       loadUserSelectedData();
     }
   }, [user?.email]);
+  
+  //delete topic
+  const deleteTopic = id => {
+    const procced = window.confirm('deleting??')
+    if(procced){
+      fetch('https://sleepy-brushlands-75204.herokuapp.com/delete/'+id, {
+      method: 'DELETE'
+    })
+    .then(res=>res.json())
+    .then(data=>console.log(data));
+    //rm from ui
+    const rest = datas.filter(data=>data?._id !== id);
+    setDatas(rest);
+    }
+  }
+  
   return (
     <div>
       <h2>selected topic {datas?.length}</h2>
@@ -30,18 +47,17 @@ const SelectedTopic = () => {
         style={{ gridTemplateColumns: "auto auto auto", gap: "5em" }}
       >
         {datas.map((data) => (
-          <div class="card">
-            <img src={data?.picture} class="card-img" alt="..." />
-            <div class="card-img-overlay">
+          <div class="card" style={{width: '18rem'}}>
+            <img src={data?.picture} class="card-img-top" alt="..." />
+            <div class="card-body">
               <h5 class="card-title">Card title</h5>
               <p class="card-text">
-                This is a wider card with supporting text below as a natural
-                lead-in to additional content. This content is a little bit
-                longer.
+                Some quick example text to build on the card title and make up
+                the bulk of the card's content.
               </p>
-              <p class="card-text">
-                <small>Last updated 3 mins ago</small>
-              </p>
+              <button onClick={()=>deleteTopic(data?._id)} class="btn btn-primary">
+                Delete trhis topic
+              </button>
             </div>
           </div>
         ))}
